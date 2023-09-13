02ffc6a789b75e6dd81ff32db4b7910d004f636c
2606399704 <144700806+2606399704@users.noreply.github.com>
2023-09-13 23:19:49 +0800

    Add files via upload

--- Project9/Project9/FileName.c
+++ Project9/Project9/FileName.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 	int i = 100;
 	int j = 2;
 	int count = 0;
-	for (i = 100; i <= 200; i++)
+	for (i = 101; i <= 200; i+=2)
 	{
-		for (j = 2; j < i; j++)
+		for (j = 2; j < sqrt(i); j++)
 			if (i % j == 0)
 				break;
-		if (i == j)
+		if (j>sqrt(i))
 		{
 			printf("%d ", i);
 			count++;
